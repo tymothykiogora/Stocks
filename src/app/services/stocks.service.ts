0f89ee714dcba127345f496a0f6167471558ea3f
tymothykiogora <67871271+tymothykiogora@users.noreply.github.com>
2022-08-14 02:17:28 +0300

    declare stock array

--- src/app/services/stocks.service.ts
+++ src/app/services/stocks.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+let stocks: Array<string> = ['AAPL', 'GOOG', 'FB', 'AMZN', 'TWTR'];
 
-@Injectable({
-  providedIn: 'root'
-})
-export class StocksService {
-
-  constructor() { }
+export interface StockInterface {
+ symbol: string;
+ lastTradePriceOnly: number;
+ change: number;
+ changeInPercent: number;
 }
+@Injectable()
+export class StocksService {
+ constructor(private http: HttpClient) {}
+ get() {
+  return stocks.slice();
+  }
+  add(stock) {
+  stocks.push(stock);
+  return this.get();
+  }
+  remove(stock) {
+  stocks.splice(stocks.indexOf(stock), 1);
+  return this.get();
+  }
+  load(symbols) {
+  if (symbols) {
+  return this.http.get<Array<StockInterface>>(service + '/stocks/
+ snapshot?symbols=' + symbols.join());
+  }
+  }
+ }
